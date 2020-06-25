f80a89f7e6e6fbd52e7f7bd19c14915d1392bf4b
Francisco Raigoza <raigoza.frankie@gmail.com>
2020-06-25 13:16:57 -0700

    Nasa App

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './component/Home';
+import NasaPhoto from './component/NasaPhoto'
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="app">
+      <Route component={Home} path="/" exact />
+      <Route component={NasaPhoto} path="/nasaphoto" />
+    </div>
+    
+    </BrowserRouter>
+  );
+}
+
+export default App;
